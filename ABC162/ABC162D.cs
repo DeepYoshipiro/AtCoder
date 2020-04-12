5076aae4666cbdfd2491eca0e6cf7f9decdaca24
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-04-12 19:45:01 +0900

    ABC162Prepared

---
+++ ABC162/ABC162D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+
+namespace ABC162
+{
+    class ABC162D
+    {
+        static void Main(string[] args)
+        {
+            // int N = int.Parse(ReadLine());
+            // char[] X = ReadLine().ToCharArray();
+
+            // int[] init = ReadLine().Split(' ')
+            //     .Select(n => int.Parse(n)).ToArray();
+            // int N = init[0];
+            // int K = init[1];
+
+            // int[] x = ReadLine().Split(' ')
+            //     .Select(n => int.Parse(n)).ToArray();
+
+            WriteLine("Hello World!");
+            ReadKey();
+        }
+    }
+}
