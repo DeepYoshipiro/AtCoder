4bb3ad1d37851863ed3a12170069e67b7850461d
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-04-22 01:44:44 +0900

    added ABC163

---
+++ ABC163/ABC163A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+
+namespace ABC163
+{
+    class ABC163A
+    {
+        static void Main(string[] args)
+        {
+            double R = double.Parse(ReadLine());
+
+            WriteLine(2 * PI * R);
+            ReadKey();
+        }
+    }
+}
---
+++ ABC163/ABC163B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+
+namespace ABC163
+{
+    class ABC163B
+    {
+        static void Main(string[] args)
+        {
+            int[] init = ReadLine().Split()
+                .Select(n => int.Parse(n)).ToArray();
+            int N = init[0];
+            int M = init[1];
+
+            int[] A = ReadLine().Split()
+                .Select(n => int.Parse(n)).ToArray();
+
+            int result = (N - A.Sum() >= 0 ? N - A.Sum() : -1);
+            WriteLine(result.ToString());
+            ReadKey();
+        }
+    }
+}
