337b7d42aefd8b9322c682f89e5818767ca5a3f2
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-04-26 18:54:10 +0900

    created ABC164 files

---
+++ ABC164/ABC164A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+
+namespace ABC164
+{
+    class ABC164A
+    {
+        static void Main(string[] args)
+        {
+            // int R = int.Parse(ReadLine());
+            
+            // int[] init = ReadLine().Split()
+            //     .Select(n => int.Parse(n)).ToArray();
+            // int N = init[0];
+            // int M = init[1];
+
+            WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ABC164/ABC164C.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+
+namespace ABC164
+{
+    class ABC164C
+    {
+        static void Main(string[] args)
+        {
+            // int R = int.Parse(ReadLine());
+            
+            // int[] init = ReadLine().Split()
+            //     .Select(n => int.Parse(n)).ToArray();
+            // int N = init[0];
+            // int M = init[1];
+
+            WriteLine("Hello World!");
+        }
+    }
+}
