05022203b24c228bf2116119061a7c48b285b15d
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-05-03 20:39:24 +0900

    created ABC166 template

---
+++ ABC166/ABC166D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC166
+{
+    class ABC166D
+    {
+        static void Main(string[] args)
+        {
+            // int N = int.Parse(ReadLine());
+            // int[] init = ReadLine().Split()
+            //     .Select(n => int.Parse(n)).ToArray();
+
+            // long N = long.Parse(ReadLine());
+            // long[] init = ReadLine().Split()
+            //     .Select(n => long.Parse(n)).ToArray();
+
+            WriteLine("Hello World!");
+            ReadKey();
+        }
+    }
+}
