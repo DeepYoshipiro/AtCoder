b9dc2f00fd41aa47c699e53d04e39bca044dbc6f
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-05-16 20:24:23 +0900

    Stack into DFS base

--- Library/DFS.cs
+++ Library/DFS.cs
@@ -12,26 +12,37 @@ namespace Library
         static void Main(string[] args)
         {
             const int M = 2;
+            var A = new Stack<int>();
             for (int a = 0; a < M; a++)
             {
+                A.Push(a);
                 for (int b = 0; b < M; b++)
                 {
+                    A.Push(b);
                     for (int c = 0; c < M; c++)
                     {
+                        A.Push(c);
                         for (int d = 0; d < M; d++)
                         {
+                            A.Push(d);
                             for (int e = 0; e < M; e++)
                             {
+                                A.Push(e);
                                 for (int f = 0; f < M; f++)
                                 {
+                                    A.Push(f);
                                     for (int g = 0; g < M; g++)
                                     {
+                                        A.Push(g);
                                         for (int h = 0; h < M; h++)
                                         {
+                                            A.Push(h);
                                             for (int i = 0; i < M; i++)
                                             {
+                                                A.Push(i);
                                                 for (int j = 0; j < M; j++)
                                                 {
+                                                    A.Push(j);
                                                     Write(a);
                                                     Write(b);
                                                     Write(c);
@@ -43,15 +54,25 @@ namespace Library
                                                     Write(i);
                                                     Write(j);
                                                     WriteLine();
+                                                    A.Pop();
                                                 }
+                                                A.Pop();
                                             }
+                                            A.Pop();
                                         }
+                                        A.Pop();
                                     }
+                                    A.Pop();
                                 }
+                                A.Pop();
                             }
+                            A.Pop();
                         }
+                        A.Pop();
                     }
+                    A.Pop();
                 }
+                A.Pop();
             }
             ReadKey();
         }
