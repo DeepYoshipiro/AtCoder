eba19e120d02c8fa563cba2cf4be864c470ae5f2
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-05-16 20:57:20 +0900

    DFS : use from Stack to List & separate Writer

--- Library/DFS.cs
+++ Library/DFS.cs
@@ -12,69 +12,68 @@ namespace Library
         static void Main(string[] args)
         {
             const int M = 2;
-            var A = new Stack<int>();
+            var A = new List<int>();
             for (int a = 0; a < M; a++)
             {
-                A.Push(a);
+                A.Add(a);
                 for (int b = 0; b < M; b++)
                 {
-                    A.Push(b);
+                    A.Add(b);
                     for (int c = 0; c < M; c++)
                     {
-                        A.Push(c);
+                        A.Add(c);
                         for (int d = 0; d < M; d++)
                         {
-                            A.Push(d);
+                            A.Add(d);
                             for (int e = 0; e < M; e++)
                             {
-                                A.Push(e);
+                                A.Add(e);
                                 for (int f = 0; f < M; f++)
                                 {
-                                    A.Push(f);
+                                    A.Add(f);
                                     for (int g = 0; g < M; g++)
                                     {
-                                        A.Push(g);
+                                        A.Add(g);
                                         for (int h = 0; h < M; h++)
                                         {
-                                            A.Push(h);
+                                            A.Add(h);
                                             for (int i = 0; i < M; i++)
                                             {
-                                                A.Push(i);
+                                                A.Add(i);
                                                 for (int j = 0; j < M; j++)
                                                 {
-                                                    A.Push(j);
-                                                    Write(a);
-                                                    Write(b);
-                                                    Write(c);
-                                                    Write(d);
-                                                    Write(e);
-                                                    Write(f);
-                                                    Write(g);
-                                                    Write(h);
-                                                    Write(i);
-                                                    Write(j);
-                                                    WriteLine();
-                                                    A.Pop();
+                                                    A.Add(j);
+                                                    Writer(A.ToList());
+                                                    A.RemoveAt(A.Count() - 1);
                                                 }
-                                                A.Pop();
+                                                A.RemoveAt(A.Count() - 1);
                                             }
-                                            A.Pop();
+                                            A.RemoveAt(A.Count() - 1);
                                         }
-                                        A.Pop();
+                                        A.RemoveAt(A.Count() - 1);
                                     }
-                                    A.Pop();
+                                    A.RemoveAt(A.Count() - 1);
                                 }
-                                A.Pop();
+                                A.RemoveAt(A.Count() - 1);
                             }
-                            A.Pop();
+                            A.RemoveAt(A.Count() - 1);
                         }
-                        A.Pop();
+                        A.RemoveAt(A.Count() - 1);
                     }
-                    A.Pop();
+                    A.RemoveAt(A.Count() - 1);
                 }
-                A.Pop();
+                A.RemoveAt(A.Count() - 1);
             }
             ReadKey();
+        }
+
+        static void Writer(List<int> A)
+        {
+            foreach (int a in A)
+            {
+                Write(a);
+            }
+            WriteLine();
         }
     }
 }
