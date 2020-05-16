c88910f063eec528433d1a3316bbd704eff324b1
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-05-16 21:14:30 +0900

    DFS : remove 9-For, remain only 1.

--- Library/DFS.cs
+++ Library/DFS.cs
@@ -9,62 +9,29 @@ namespace Library
 {
     class DFSTest
     {
+        const int M = 2;
+
         static void Main(string[] args)
         {
-            const int M = 2;
             var A = new List<int>();
-            for (int a = 0; a < M; a++)
+            DFS(A);
+            ReadKey();
+        }
+
+        static void DFS(List<int> A)
+        {
+            if (A.Count() == 10)
             {
-                A.Add(a);
-                for (int b = 0; b < M; b++)
-                {
-                    A.Add(b);
-                    for (int c = 0; c < M; c++)
-                    {
-                        A.Add(c);
-                        for (int d = 0; d < M; d++)
-                        {
-                            A.Add(d);
-                            for (int e = 0; e < M; e++)
-                            {
-                                A.Add(e);
-                                for (int f = 0; f < M; f++)
-                                {
-                                    A.Add(f);
-                                    for (int g = 0; g < M; g++)
-                                    {
-                                        A.Add(g);
-                                        for (int h = 0; h < M; h++)
-                                        {
-                                            A.Add(h);
-                                            for (int i = 0; i < M; i++)
-                                            {
-                                                A.Add(i);
-                                                for (int j = 0; j < M; j++)
-                                                {
-                                                    A.Add(j);
-                                                    Writer(A.ToList());
-                                                    A.RemoveAt(A.Count() - 1);
-                                                }
-                                                A.RemoveAt(A.Count() - 1);
-                                            }
-                                            A.RemoveAt(A.Count() - 1);
-                                        }
-                                        A.RemoveAt(A.Count() - 1);
-                                    }
-                                    A.RemoveAt(A.Count() - 1);
-                                }
-                                A.RemoveAt(A.Count() - 1);
-                            }
-                            A.RemoveAt(A.Count() - 1);
-                        }
-                        A.RemoveAt(A.Count() - 1);
-                    }
-                    A.RemoveAt(A.Count() - 1);
-                }
+                Writer(A.ToList());
+                return;
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                A.Add(i);
+                DFS(A);
                 A.RemoveAt(A.Count() - 1);
             }
-            ReadKey();
         }
 
         static void Writer(List<int> A)
