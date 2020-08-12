6bc7ba06f55ec615f9a2c19a51d38d69e2f0d630
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-08-13 01:16:02 +0900

    Solved ABC157D

--- ABC157/ABC157D.cs
+++ ABC157/ABC157D.cs
@@ -11,65 +11,106 @@ namespace ABC157
     {
         static void Main(string[] args)
         {
-            // var init = ReadLine().Split()
-            //         .Select(n => int.Parse(n)).ToArray();
-            // var N = init[0];
-            int N = 10;
-            // var M = init[1];
-            // //int K = init[2];
+            var init = ReadLine().Split()
+                    .Select(n => int.Parse(n)).ToArray();
+            var N = init[0];
+            var M = init[1];
+            var K = init[2];
             
-            // var friend = new List<int>[N + 1];
-            // for (int j = 1; j <= N; j++)
-            // {
-            //     friend[j] = new List<int>();
-            // } 
+            var friend = new List<int>[N + 1];
+            for (int j = 1; j <= N; j++)
+            {
+                friend[j] = new List<int>();
+            } 
 
             var uf = new Union_Find(N);
-            WriteLine(uf.root(5));
-            ReadKey();
-            // for (int i = 0; i < M; i++)
-            // {
-            //     var like = ReadLine().Split()
-            //         .Select(n => int.Parse(n)).ToArray();
-            //     var x = like[0];
-            //     var y = like[1];
+            for (int i = 0; i < M; i++)
+            {
+                var like = ReadLine().Split()
+                    .Select(n => int.Parse(n)).ToArray();
+                var x = like[0];
+                var y = like[1];
                 
-            //     friend[x].Add(y);
-            //     friend[y].Add(x);
+                friend[x].Add(y);
+                friend[y].Add(x);
 
-            //     uf.Union(x, y);
-            // }
+                uf.Union(x, y);
+            }
+
+            var groupMemberCnt = new int[N + 1];
+            var friendCandidateCnt = new int[N + 1];
+            for (int j = 1; j <= N; j++)
+            {
+                uf.Root(j);
+            }
+            
+            for (int j = 1; j <= N; j++)
+            {
+                groupMemberCnt[uf.Parent[j]]++;
+            }
+            
+            for (int j = 1; j <= N; j++)
+            {
+                if (j != uf.Parent[j])
+                {
+                    groupMemberCnt[j]
+                     = groupMemberCnt[uf.Parent[j]];
+                }
+                friendCandidateCnt[j] = groupMemberCnt[j] - 1;
+                friendCandidateCnt[j] -= friend[j].Count();
+            }
+
+            for (int i = 0; i < K; i++)
+            {
+                var block = ReadLine().Split()
+                    .Select(n => int.Parse(n)).ToArray();
+                var x = block[0];
+                var y = block[1];
+
+                if (uf.Parent[x] == uf.Parent[y])
+                { 
+                    friendCandidateCnt[x]--;
+                    friendCandidateCnt[y]--;
+                }                    
+            }
+
+            for (int j = 1; j <= N; j++)
+            {
+                Write(friendCandidateCnt[j] + " ");
+            }
+            WriteLine();
+            ReadKey();
         }
-
     }
+
     internal class Union_Find
     {
-        int[] parent;
+        internal int[] Parent;
         internal Union_Find(int N)
         {
-            parent = new int[N + 1];
+            Parent = new int[N + 1];
             for (int i = 1; i <= N; i++)
             {
-                parent[i] = i - 1;  // for test
-                // parent[i] = i;
+                Parent[i] = i;
             }
-            parent[1] = 1;
         }
 
         internal void Union(int x, int y)
         {
-            int p = root(x);
-            int q = root(y);
+            int p = Root(x);
+            int q = Root(y);
 
             if (p == q) return;
+            Parent[q] = p;
+            Parent[y] = p;
 
-                    
+            return;  
         }
 
-        internal int root(int x)
+        internal int Root(int x)
         {
-            if (x == parent[x]) return x;
-            return parent[x] = root(parent[x]);
+            if (x == Parent[x]) return x;
+            return Parent[x] = Root(Parent[x]);
         }
     }
 }
