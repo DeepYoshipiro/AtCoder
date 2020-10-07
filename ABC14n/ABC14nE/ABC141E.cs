6b790dd474cb2f16b0f3c995cada9e60dd7211b8
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-10-07 22:12:01 +0900

    Understood & Solved ABC141E

--- ABC14n/ABC14nE/ABC141E.cs
+++ ABC14n/ABC14nE/ABC141E.cs
@@ -1,62 +1,4 @@
-﻿// using System;
-// using System.Linq;
-// using System.Collections.Generic;
-// using System.Text;
-// using static System.Console;
-// using static System.Math;
- 
-// namespace ABC141
-// {
-//     class ABC141E
-//     {
-//         static void Main(string[] args)
-//         {
-//             var N = int.Parse(ReadLine());
-//             string S = ReadLine();
-//             var Letter = new List<int>['z' - 'a' + 1]
-//                 .Select(v => new List<int>()).ToArray();
- 
- 
-//             for (int i = 0; i < N; i++) 
-//             {
-//                 Letter[char.Parse(S.Substring(i, 1)) - 'a'].Add(i);
-//             }
- 
-//             int len = 0;
-//             for (int l = 0; l < 26; l++)
-//             {
-//                 if (Letter[l].Count() >= 2)
-//                 {
-//                     len = 1;
-//                     break;
-//                 } 
-//             }
- 
-//             for (int l = 0; l < 26; l++)
-//             {
-//                 if (Letter[l].Count() <= 1) continue;
-//                 for (int i = 0; i < Letter[l].Count() - 1; i++)
-//                 {
-//                     for (int j = i + 1; j < Letter[l].Count(); j++)
-//                     {
-//                         var l1 = Letter[l][i];
-//                         var l2 = Letter[l][j];
- 
-//                         while (l2 + len < N && l1 + len < l2)
-//                         {
-//                             if (S.Substring(l1, len + 1) != S.Substring(l2, len + 1)) break;
-//                             len++;
-//                         }
-//                     }
-//                 }
-//             }
- 
-//             WriteLine(len.ToString());
-//             ReadKey();
-//         }
-//     }
-// }
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
@@ -74,42 +16,27 @@ namespace ABC141
             var Letter = new List<int>['z' - 'a' + 1]
                 .Select(v => new List<int>()).ToArray();
 
-
             for (int i = 0; i < N; i++) 
             {
                 Letter[char.Parse(S.Substring(i, 1)) - 'a'].Add(i);
             }
 
-            int len = 0;
-            for (int l = 0; l < 26; l++)
+            var dp = new int[N + 1][];
+            dp[N] = new int[N];
+            int result = 0;
+            for (int i = N - 1; i > 0; i--)
             {
-                if (Letter[l].Count() >= 2)
+                dp[i] = new int[i];
+                var L = Letter[char.Parse(S.Substring(i, 1)) - 'a'];
+                foreach (int j in L)
                 {
-                    len = 1;
-                    break;
-                } 
-            }
-
-            for (int l = 0; l < 26; l++)
-            {
-                if (Letter[l].Count() <= 1) continue;
-                for (int i = 0; i < Letter[l].Count() - 1; i++)
-                {
-                    for (int j = i + 1; j < Letter[l].Count(); j++)
-                    {
-                        var l1 = Letter[l][i];
-                        var l2 = Letter[l][j];
-
-                        while (l2 + len < N && l1 + len < l2)
-                        {
-                            if (S.Substring(l1, len + 1) != S.Substring(l2, len + 1)) break;
-                            len++;
-                        }
-                    }
+                    if (j >= i) break;
+                    dp[i][j] = Min(dp[i + 1][j + 1] + 1, i - j);
+                    if (dp[i][j] > result) result = dp[i][j];
                 }
             }
 
-            WriteLine(len.ToString());
+            WriteLine(result.ToString());
             ReadKey();
         }
     }
