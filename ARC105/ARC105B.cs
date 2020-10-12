9b7dcf4f4bb5e8a0f40cb0f3c9bc85275a80ee8c
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-10-12 10:38:09 +0900

    Corrected ARC105B

--- ARC105/ARC105B.cs
+++ ARC105/ARC105B.cs
@@ -13,65 +13,19 @@ namespace ARC105
         {
             var N = int.Parse(ReadLine());
             var A = ReadLine().Split()
-                .Select(n => int.Parse(n))
+                .Select(n => long.Parse(n))
                 .Distinct()
-                .OrderBy(n => n)
+                .OrderByDescending(n => n)
                 .ToList();
 
-            var dic = new SortedDictionary<int, int>();
-            for (int i = 0; i < A.Count() - 1; i++)
+            long result = A.First();
+            var dm = new DiscreteMath();
+            for (int i = 0; i < A.Count(); i++)
             {
-                if (dic.ContainsKey(A[i]))
-                {
-                    dic[A[i]]++;
-                }
-                else
-                {
-                    dic.Add(A[i], 1);
-                }
+                result = dm.GCD(result, A[i]);
             }
 
-            var max = dic.Last().Key;
-            var min = dic.First().Key;
-
-            while (dic.Count() > 1)
-            {
-                if (dic.ContainsKey(max - min))
-                {
-                    dic[max - min] += dic[max];
-                    dic.Remove(max);
-                }
-                else
-                {
-                    dic.Add(max - min, dic[max]);
-                    dic.Remove(max);
-                }
-                max = dic.Last().Key;
-                min = dic.First().Key;
-            }
-            // var B = new List<int>();
-
-            // var max = A.Last();
-            // var min = A.First();
-            // var maxCur = A.Count() - 1;
-            // var minCur = 0;
-            // var cur = max - min;
-            // while (max > min)
-            // {
-            //     A.RemoveAt(A.Count() - 1);
-            //     A.Add(cur);
-            //     A.Sort();
-            //     max = A.Last();
-            //     min = A.First();
-            //     cur = max - min;
-            // }
-            // long result = A.First();
-            // var dm = new DiscreteMath();
-            // for (int i = 0; i < A.Count() - 1; i++)
-            // {
-            //     result = dm.GCD(A[i], A[i + 1]);
-            // }
-            WriteLine(dic.First().Key.ToString());
+            WriteLine(result.ToString());
             ReadKey();
         }
 
