7e7ad03af3110902ff4a1f023a30600c2caab791
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2020-11-04 02:44:21 +0900

    solved ABC181C

--- ABC181/ABC181C.cs
+++ ABC181/ABC181C.cs
@@ -13,12 +13,12 @@ namespace ABC181
         {
             var N = int.Parse(ReadLine());
             
-            var x = new double[N];
-            var y = new double[N];
+            var x = new int[N];
+            var y = new int[N];
             for (int i = 0; i < N; i++)
             {
                 var point = ReadLine().Split()
-                    .Select(n => double.Parse(n)).ToArray();
+                    .Select(n => int.Parse(n)).ToArray();
                 x[i] = point[0];
                 y[i] = point[1];
             }
@@ -27,36 +27,19 @@ namespace ABC181
             {
                 for (int j = i + 1; j < N; j++)
                 {
-                    double m1 = (x[j] - x[i] == 0
-                         ? 0 : (y[j] - y[i]) / (x[j] - x[i]));
+                    var x1 = x[j] - x[i];
+                    var y1 = y[j] - y[i];
                     for (int k = j + 1; k < N; k++)
                     {
-                        if (x[j] - x[i] == 0 && x[k] - x[i] == 0)
+                        var x2 = x[k] - x[i];
+                        var y2 = y[k] - y[i];
+                        var det = x1 * y2 - y1 * x2;
+                        if (det == 0)
                         {
                             WriteLine("Yes");
                             ReadKey();
                             return;
                         }     
-                        double m2 = (x[k] - x[i] == 0
-                            ? 0
-                            : (y[k] - y[i]) / (x[k] - x[i]));
-                        if (m1 != m2) continue;
-                        var m = m1;
-                        if (m == 0)
-                        {
-                            WriteLine("Yes");
-                            ReadKey();
-                            return;
-                        }
-
-                        var b1 = -m * x[k] + y[k];
-                        var b2 = -m * x[i] + y[i];
-                        if (b1 == b2)
-                        {
-                            WriteLine("Yes");
-                            ReadKey();
-                            return;
-                        }
                     }
                 }
             }
