841430adab587d2d093f4590b0fea41d277e8f5c
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2021-01-10 20:26:24 +0900

    created ABC188

---
+++ ABC188/ABC188E.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC188
+{
+    class ABC188E
+    {
+        static void Main(string[] args)
+        {
+            // var N = int.Parse(ReadLine());
+            // var init = ReadLine().Split()
+            //     .Select(n => int.Parse(n)).ToArray();
+            // var N = init[0];
+            // var M = init[1];
+
+            // var N = long.Parse(ReadLine());
+            // var init = ReadLine().Split()
+            //     .Select(n => long.Parse(n)).ToArray();
+            // var N = init[0];
+            // var M = init[1];
+
+            // var S = ReadLine().ToCharArray();
+            // var S = ReadLine();
+            // var S = ReadLine().ToArray();
+
+            WriteLine("Welcome to AtCoder!!");
+            ReadKey();
+        }
+    }
+}
