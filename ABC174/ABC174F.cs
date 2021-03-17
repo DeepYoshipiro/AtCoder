156bb9113c04461461ee1a5aa7e53ce17a1675e2
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2021-03-18 00:37:11 +0900

    ABC021 solving (before refactoring)

---
+++ ABC174/ABC174F.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC174
+{
+    class ABC174F
+    {
+        static void Main(string[] args)
+        {
+            // var N = int.Parse(ReadLine());
+            var init = ReadLine().Split()
+                .Select(n => int.Parse(n)).ToArray();
+            var N = init[0];
+            var Q = init[1];
+
+            var C = (new int[]{0})
+                .Concat(ReadLine().Split().Select(n => int.Parse(n)))
+                .ToArray();
+
+            var rsq = new RSQ(N + 1);
+            for (int i = 1; i <= N; i++)
+            {
+                rsq.Update(i, C[i]);
+            }
+
+            // var N = long.Parse(ReadLine());
+            // var init = ReadLine().Split()
+            //     .Select(n => long.Parse(n)).ToArray();
+            // var N = init[0];
+            // var M = init[1];
+
+            // var S = ReadLine().ToCharArray();
+            // var S = ReadLine();
+            // var S = ReadLine().ToArray();
+
+            WriteLine("Welcome to AtCoder!!");
+            ReadKey();
+        }
+
+        internal class RSQ
+        {
+            private int N;
+            private int n;
+            private int[] sum;
+
+            internal RSQ(int len)
+            {
+                n = 1;
+                while (n < len)
+                {
+                    n *= 2;
+                }
+                N = 2 * n - 1;
+
+                sum = new int[N];
+            }
+
+            internal Update(int idx, int val)
+            {
+                idx += n - 1;
+                sum[idx] = val
+
+                while (idx > 0)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Classics/CyanProblems/ABC021C.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace CyanProblems
+{
+    class ABC021C
+    {
+        static internal HashSet<int>[] parent;
+        static internal int A;
+        static internal long[] dp;
+        static internal bool[] calculated;
+
+        static void Main(string[] args)
+        {
+            var N = int.Parse(ReadLine());
+            var fromTo = ReadLine().Split()
+                .Select(n => int.Parse(n)).ToArray();
+            A = fromTo[0];
+            var B = fromTo[1];
+
+            var M = int.Parse(ReadLine());
+
+            var road = new List<int>[N + 1]
+                .Select(v => new List<int>()).ToArray();
+            for (int i = 0; i < M; i++)
+            {
+                var input = ReadLine().Split()
+                    .Select(n => int.Parse(n)).ToArray();
+                var X = input[0];
+                var Y = input[1];
+
+                road[X].Add(Y);
+                road[Y].Add(X);
+            }
+
+            var searched = new bool[N + 1];
+            parent = new HashSet<int>[N + 1]
+                .Select(v => new HashSet<int>()).ToArray();
+            var dist = Enumerable.Repeat<int>(int.MaxValue / 2, N + 1)
+                .ToArray();
+            dist[A] = 0;
+
+            var que = new Queue<int>();
+            que.Enqueue(A);
+
+            while (que.Count() > 0)
+            {
+                var cur = que.Dequeue();
+                if (searched[cur]) continue;
+                if (cur == B) break;
+
+                foreach (int next in road[cur])
+                {
+                    if (searched[next]) continue;
+                    switch(dist[next].CompareTo(dist[cur] + 1))
+                    {
+                        case 1:
+                            parent[next].Clear();
+                            parent[next].Add(cur);
+                            dist[next] = dist[cur] + 1;
+                            que.Enqueue(next);
+                            break;
+                        case 0:
+                            parent[next].Add(cur);
+                            que.Enqueue(next);
+                            break;
+                        default:
+                            break;
+                    };
+                    searched[cur] = true;
+                }
+            }
+
+            long MOD = 1000000000 + 7;
+            dp = new long[N + 1];
+            calculated = new bool[N + 1];
+            var dpQue = new Queue<long>();
+            dp[B] = 1;
+            dpQue.Enqueue(B);
+            while (dpQue.Count() > 0)
+            {
+                var cur = dpQue.Dequeue();
+                if (calculated[cur]) continue;
+
+                foreach (int prev in parent[cur])
+                {
+                    dp[prev] += dp[cur];
+                    dp[prev] %= MOD;
+                    dpQue.Enqueue(prev);
+                }
+                calculated[cur] = true;
+            }
+            // DFS(B);
+
+            var result = dp[A];
+
+            WriteLine(result.ToString());
+            ReadKey();
+        }
+
+        static void DFS(int cur)
+        {
+            if (cur == A) return;
+            // long result = 1;
+            foreach (int prev in parent[cur])
+            {
+                dp[prev] += dp[cur];
+                DFS(prev);
+            }
+        }
+    }
+}
