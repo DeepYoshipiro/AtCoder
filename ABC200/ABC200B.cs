c680dba62e35e243baaf4a812c3512b48c628b27
DeepYoshipiro <yoshipiro.takahashi@mbr.nifty.com>
2021-05-09 04:32:33 +0900

    ended ABC200

---
+++ ABC200/ABC200B.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC200
+{
+    class ABC200B
+    {
+        static void Main(string[] args)
+        {
+            var init = ReadLine().Split()
+                .Select(n => long.Parse(n)).ToArray();
+            var N = init[0];
+            var K = (int)init[1];
+
+            for (int i = 0; i < K; i++)
+            {
+                if (N % 200 == 0)
+                {
+                    N /= 200;
+                }
+                else
+                {
+                    N *= 1000;
+                    N += 200;
+                }
+            }
+
+            WriteLine(N.ToString());
+            ReadKey();
+        }
+    }
+}
---
+++ ABC200/ABC200C.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC200
+{
+    class ABC200C
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(ReadLine());
+
+            var R = new long[200];
+            
+            var A = ReadLine().Split()
+                .Select(n => int.Parse(n)).ToArray();
+            for (int i = 0; i < N; i++)
+            {
+                R[A[i] % 200]++;
+            }
+
+            long result = 0;
+            for (int j = 0; j < 200; j++)
+            {
+                if (R[j] <= 1) continue;
+                result += (R[j] * (R[j] - 1)) / 2;
+            }
+
+            WriteLine(result.ToString());
+            ReadKey();
+        }
+    }
+}
