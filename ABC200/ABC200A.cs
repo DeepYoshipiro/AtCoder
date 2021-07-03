8532803e9d3bde1a1609a4e33de1df9e31209b0f
DeepYoshipiro <40316412+DeepYoshipiro@users.noreply.github.com>
2021-07-03 11:28:41 +0900

    created ABC200

---
+++ ABC200/ABC200A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace ABC200
+{
+    class ABC200A
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(ReadLine());
+
+            WriteLine((N + 99) / 100);
+            ReadKey();
+        }
+    }
+}
